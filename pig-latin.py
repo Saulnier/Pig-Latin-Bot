6581ca43667f30c38d0cff27910f6a9da92f9cfd
Adam Saulnier <adam.saulnier@fourwindsinteractive.com>
2018-11-01 00:42:32 -0600

    update to fourwinds

--- pig-latin.py
+++ pig-latin.py
@@ -1,12 +1,12 @@
-pyg = 'ay'
+pyg = ' fourwinds'
 
 original = input('Enter a word:') #WORD
 
 if len(original) > 0 and original.isalpha():  # adding alpha char
   word = original.lower() #word
   first = word[0] #w
-  new_word = word + first + pyg #wordway
-  new_word = new_word[1:len(new_word)] #ordway
+  new_word = word  + pyg #wordway + first
+  #new_word = new_word[1:len(new_word)] #ordway
   print (new_word)
 else:
     print ('please type one word')
